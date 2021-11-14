d4750d7b91cddf9e68081cd941a96f3104cee2dd
Smiix <kenjy.thiebault@gmail.com>
2021-11-14 14:32:07 +0100

    Feat(back/blog): listing & update - Add tests bad data

--- tests/Controller/Admin/BlogControllerTest.php
+++ tests/Controller/Admin/BlogControllerTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\Controller\Admin;
 
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 final class BlogControllerTest extends WebTestCase
 {
@@ -44,7 +45,7 @@ final class BlogControllerTest extends WebTestCase
 
         $client->request('GET', '/admin/blog/' . $post->getId());
         $client->submitForm('Modifier', $data);
-        self::assertResponseStatusCodeSame(302);
+        self::assertResponseStatusCodeSame(Response::HTTP_FOUND);
 
         $updatedPost = $repository->findOneBy(['id' => $post->getId()]);
         $client->followRedirect();
@@ -56,6 +57,29 @@ final class BlogControllerTest extends WebTestCase
         self::assertSelectorTextSame('.flash-success', 'Le post a bien été mis à jour.');
     }
 
+    /**
+     * @dataProvider generateBadData
+     */
+    public function testUpdatePostWithBadData(array $data, string $errorMessage): void
+    {
+        $client = self::createClient();
+
+        $repository = $client->getContainer()->get(PostRepository::class);
+        $post = $repository->findOneBy([]);
+
+        $client->request('GET', '/admin/blog/' . $post->getId());
+        $client->submitForm('Modifier', $data);
+
+        self::assertResponseStatusCodeSame(Response::HTTP_OK);
+        $updatedPost = $repository->findOneBy(['id' => $post->getId()]);
+
+        self::assertEquals($post->getTitle(), $updatedPost->getTitle());
+        self::assertEquals($post->getContent(), $updatedPost->getContent());
+        self::assertEquals($post->getSummary(), $updatedPost->getSummary());
+        self::assertEquals($post->isOnline(), $updatedPost->isOnline());
+        self::assertSelectorTextSame('ul.form__errors > li', $errorMessage);
+    }
+
     public function generateUri(): \Generator
     {
         yield [
@@ -65,6 +89,64 @@ final class BlogControllerTest extends WebTestCase
         yield [
             'GET',
             '/admin/blog/25'
+        ];
+    }
+
+    public function generateBadData(): \Generator
+    {
+        yield [
+            [
+                'post[title]' => '',
+                'post[summary]' => 'summary test',
+                'post[content]' => 'content test',
+                'post[online]' => false,
+            ],
+            'This value should not be blank.'
+        ];
+        yield [
+            [
+                'post[title]' => '12345',
+                'post[summary]' => 'summary test',
+                'post[content]' => 'content test',
+                'post[online]' => false,
+            ],
+            'This value is too short. It should have 6 characters or more.'
+        ];
+        yield [
+            [
+                'post[title]' => 'title test',
+                'post[summary]' => '',
+                'post[content]' => 'content test',
+                'post[online]' => false,
+            ],
+            'This value should not be blank.'
+        ];
+        yield [
+            [
+                'post[title]' => 'title test',
+                'post[summary]' => '12345678',
+                'post[content]' => 'content test',
+                'post[online]' => false,
+            ],
+            'This value is too short. It should have 10 characters or more.'
+        ];
+        yield [
+            [
+                'post[title]' => 'title test',
+                'post[summary]' => 'summary test',
+                'post[content]' => '',
+                'post[online]' => false,
+            ],
+            'This value should not be blank.'
+        ];
+        yield [
+            [
+                'post[title]' => 'title test',
+                'post[summary]' => 'summary test',
+                'post[content]' => '12345678',
+                'post[online]' => false,
+            ],
+            'This value is too short. It should have 10 characters or more.'
         ];
     }
 }
